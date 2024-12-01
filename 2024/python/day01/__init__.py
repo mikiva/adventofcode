98e8b94ced4be4b89a713b1b1c71af9e3be340d7
Mikael Ivarsson <mikael.ivarsson4@gmail.com>
2024-12-01 22:42:23 +0100

    cleanup

--- 2024/python/day01/__init__.py
+++ 2024/python/day01/__init__.py
@@ -14,22 +14,22 @@ def test():
     run(ex_input)
 
 def run(input: str):
-    part1(input)
-    part2(input)
-
-
-def part1(input: str):
     lines = input.strip().split("\n")
     lines = list(l.split("   ") for l in lines)
     left = sorted(list(int(item[0]) for item in lines))
     right = sorted(list(int(item[1]) for item in lines))
     sorted_lines = list(zip(left,right))
+    part1(sorted_lines)
+    part2(sorted_lines)
+
+
+def part1(sorted_lines: list):
     distance = sum((abs(line[0] - line[1])) for line in sorted_lines)
     print(f'1: {distance}')
 
-def part2(input: str):
-    lines = input.strip().split("\n")
-    lines = list(l.split("   ") for l in lines)
+def part2(lines: list):
+    #lines = input.strip().split("\n")
+    #lines = list(l.split("   ") for l in lines)
     left, right = Counter(int(item[0]) for item in lines), Counter(int(item[1]) for item in lines)
     left, right = Counter(left), Counter(right)
     occurences = 0
--- 2024/python/main.py
+++ 2024/python/main.py
@@ -1,11 +1,9 @@
 from sys import argv
-from subprocess import Popen, PIPE
 import git
 from requests import get
 
 day = argv[1]
 example = len(argv) > 2
-
 
 def get_input():
     url = f"https://adventofcode.com/2024/day/{day}/input"
@@ -16,15 +14,8 @@ def get_input():
 
 
 if __name__ == "__main__":
-    print("hello", day)
-    #file_name = 
-    #p = Popen(["git", "rev-parse", "--show-toplevel"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    #root, err = p.communicate()
     git_repo = git.Repo(".", search_parent_directories=True)
     root = git_repo.working_tree_dir
-
-    #print(root)
-    
     package_name = f'day{day if len(day) > 1 else "0" + day}'
     package_day = __import__(package_name)
     if example:
